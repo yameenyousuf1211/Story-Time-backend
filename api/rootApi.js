c29c721a4d342374b3a40f72d5f05e9803cc4d16
Shahzad <shazee257@gmail.com>
2023-12-25 03:35:59 +0500

    cities states countries APIs created

--- api/rootApi.js
+++ api/rootApi.js
@@ -1,16 +1,18 @@
-const { Router } = require('express')
-const { DefaultHandler } = require('../controllers/rootController');
+const router = require('express').Router();
+const { DefaultHandler, getCountries, getStates, getCitiesByState } = require('../controllers/rootController');
 
 class RootAPI {
     constructor() {
-        this.router = Router();
+        this.router = router;
         this.setupRoutes();
     }
 
     setupRoutes() {
-        const router = this.router;
-        
         router.get('/', DefaultHandler);
+        router.get('/countries', getCountries);
+
+        router.get('/states', getStates);
+        router.get('/cities', getCitiesByState);
     }
 
     getRouter() {

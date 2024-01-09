44d57815b2c479fe63ab86508b4ccf4dbe6a59c3
Shahzad <shazee257@gmail.com>
2024-01-10 04:05:19 +0500

    guideline API changes

--- validations/guidelineValidation.js
+++ validations/guidelineValidation.js
@@ -5,15 +5,16 @@ exports.addGuidelineValidation = Joi.object({
   type: Joi.string().valid(...Object.values(GUIDELINE)).required(),
   content: Joi.string().required(),
   title: Joi.string().allow(null, ""),
-  _id: Joi.string().when('type', {
-    is: GUIDELINE.FAQS,
-    then: Joi.when('_id', {
-      is: Joi.exist(),
-      then: Joi.required(),
-      otherwise: Joi.optional()
-    }),
-    otherwise: Joi.optional()
-  })
+  faqId: Joi.string().optional().allow(null, ""),
+  // _id: Joi.string().when('type', {
+  //   is: GUIDELINE.FAQS,
+  //   then: Joi.when('_id', {
+  //     is: Joi.exist(),
+  //     then: Joi.required(),
+  //     otherwise: Joi.optional()
+  //   }),
+  //   otherwise: Joi.optional()
+  // })
 });
 
 exports.getGuidelineValidation = Joi.object({

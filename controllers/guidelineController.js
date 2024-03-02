a66a7a9ce29de73ecde9ff84e82b1360adc96bba
Shahzad <shazee257@gmail.com>
2024-03-02 07:36:39 +0500

    Refactor controller functions to use asyncHandler

--- controllers/guidelineController.js
+++ controllers/guidelineController.js
@@ -1,10 +1,10 @@
 const { createGuideline, deleteGuidelineById, findGuideline, createOrUpdateGuideline, getAllGuidelines } = require("../models/guidelineModel");
-const { parseBody, generateResponse } = require("../utils");
+const { parseBody, generateResponse, asyncHandler } = require("../utils");
 const { addGuidelineValidation, getGuidelineValidation } = require("../validations/guidelineValidation");
 const { STATUS_CODES, GUIDELINE } = require("../utils/constants");
 
 // add guidelines 
-exports.addGuidelines = async (req, res, next) => {
+exports.addGuidelines = asyncHandler(async (req, res, next) => {
     const body = parseBody(req.body);
 
     // Joi Validation
@@ -16,25 +16,22 @@ exports.addGuidelines = async (req, res, next) => {
 
     let result, successMessage;
 
-    try {
-        if (body.type === GUIDELINE.FAQS) {
-            result = body.faqId
-                ? await createOrUpdateGuideline({ _id: body.faqId, type: GUIDELINE.FAQS }, body)
-                : await createGuideline(body);
+    if (body.type === GUIDELINE.FAQS) {
+        result = body.faqId
+            ? await createOrUpdateGuideline({ _id: body.faqId, type: GUIDELINE.FAQS }, body)
+            : await createGuideline(body);
 
-            successMessage = body.faqId ? 'FAQ updated successfully' : 'FAQ created successfully';
-        } else {
-            result = await createOrUpdateGuideline({ type: body.type }, body);
-            successMessage = `${body.type} created`;
-        }
-
-        generateResponse(result, successMessage, res);
-    } catch (error) {
-        next(error);
+        successMessage = body.faqId ? 'FAQ updated successfully' : 'FAQ created successfully';
+    } else {
+        result = await createOrUpdateGuideline({ type: body.type }, body);
+        successMessage = `${body.type} created`;
     }
-}
 
-exports.getGuidelines = async (req, res, next) => {
+    generateResponse(result, successMessage, res);
+});
+
+// get guidelines
+exports.getGuidelines = asyncHandler(async (req, res, next) => {
     const page = parseInt(req.query?.page) || 1;
     const limit = parseInt(req.query?.limit) || 10;
     const { type = null } = req.query;
@@ -47,31 +44,24 @@ exports.getGuidelines = async (req, res, next) => {
         message: error.details[0].message
     });
 
-    try {
-        const guideline = await getAllGuidelines({ query, page, limit });
-        if (guideline?.guidelines?.length === 0) {
-            return generateResponse(null, `No ${type} Found`, res);
-        }
-
-        generateResponse(guideline, `${type} Found`, res);
-    } catch (error) {
-        next(error)
+    const guideline = await getAllGuidelines({ query, page, limit });
+    if (guideline?.guidelines?.length === 0) {
+        return generateResponse(null, `No ${type} Found`, res);
     }
-}
 
-exports.deleteGuideline = async (req, res, next) => {
+    generateResponse(guideline, `${type} Found`, res);
+});
+
+// delete guidelines
+exports.deleteGuideline = asyncHandler(async (req, res, next) => {
     const { guidelineId } = req.params;
 
-    try {
-        const guidelineObj = await findGuideline({ _id: guidelineId });
-        if (!guidelineObj) return next({
-            statusCode: STATUS_CODES.NOT_FOUND,
-            message: 'Guideline not found',
-        });
+    const guidelineObj = await findGuideline({ _id: guidelineId });
+    if (!guidelineObj) return next({
+        statusCode: STATUS_CODES.NOT_FOUND,
+        message: 'Guideline not found',
+    });
 
-        await deleteGuidelineById(guidelineId);
-        generateResponse(guidelineObj, "Deleted successfully", res);
-    } catch (error) {
-        next(error)
-    }
-}
+    await deleteGuidelineById(guidelineId);
+    generateResponse(guidelineObj, "Deleted successfully", res);
+});
--- controllers/storyController.js
+++ controllers/storyController.js
@@ -7,7 +7,7 @@ const { createComment, removeCommentById, getCommentById, getAllComments, update
 const { Types } = require('mongoose');
 
 //Create Text Story
-exports.createStory = async (req, res, next) => {
+exports.createStory = asyncHandler(async (req, res, next) => {
     const body = parseBody(req.body);
 
     // Joi validation
@@ -17,35 +17,27 @@ exports.createStory = async (req, res, next) => {
         message: error.details[0].message
     });
 
-    try {
-        // create story in db
-        const story = await createStory(body);
-        generateResponse(story, 'Story created successfully', res);
-    } catch (error) {
-        next(error)
-    }
-}
+    // create story in db
+    const story = await createStory(body);
+    generateResponse(story, 'Story created successfully', res);
+});
 
 // get all stories
-exports.fetchAllStories = async (req, res, next) => {
+exports.fetchAllStories = asyncHandler(async (req, res, next) => {
     const user = req.user.id;
     const page = req.query.page || 1;
     const limit = req.query.limit || 10;
 
     const query = getStoriesQuery(user);
 
-    try {
-        const storiesData = await getAllStories({ query, page, limit });
-        if (storiesData?.stories?.length === 0) {
-            generateResponse(null, 'No any story found', res);
-            return;
-        }
-
-        generateResponse(storiesData, 'All stories retrieved successfully', res);
-    } catch (error) {
-        next(error);
+    const storiesData = await getAllStories({ query, page, limit });
+    if (storiesData?.stories?.length === 0) {
+        generateResponse(null, 'No any story found', res);
+        return;
     }
-}
+
+    generateResponse(storiesData, 'All stories retrieved successfully', res);
+});
 
 // get user's stories
 exports.fetchUserStories = asyncHandler(async (req, res, next) => {
@@ -71,7 +63,7 @@ exports.fetchUserStories = asyncHandler(async (req, res, next) => {
 });
 
 // get a story by Id
-exports.fetchStoryById = async (req, res, next) => {
+exports.fetchStoryById = asyncHandler(async (req, res, next) => {
     const { storyId } = req.params;
 
     // check if ID is valid
@@ -80,26 +72,22 @@ exports.fetchStoryById = async (req, res, next) => {
         message: 'Please, provide storyId properly.'
     });
 
-    try {
-        const story = await findStoryById(storyId).populate('creator subCategory').lean();
-        story.likesCount = story.likes.length;
-        story.dislikesCount = story.dislikes.length;
-        story.likedByMe = story.likes.includes(new Types.ObjectId(req.user.id));
-        story.dislikesByMe = story.dislikes.includes(new Types.ObjectId(req.user.id));
+    const story = await findStoryById(storyId).populate('creator subCategory').lean();
+    story.likesCount = story.likes.length;
+    story.dislikesCount = story.dislikes.length;
+    story.likedByMe = story.likes.includes(new Types.ObjectId(req.user.id));
+    story.dislikesByMe = story.dislikes.includes(new Types.ObjectId(req.user.id));
 
-        if (!story) return next({
-            statusCode: STATUS_CODES.NOT_FOUND,
-            message: 'Story not found'
-        });
+    if (!story) return next({
+        statusCode: STATUS_CODES.NOT_FOUND,
+        message: 'Story not found'
+    });
 
-        generateResponse(story, 'Story retrieved successfully', res);
-    } catch (error) {
-        next(error);
-    }
-}
+    generateResponse(story, 'Story retrieved successfully', res);
+});
 
 // like a story
-exports.likeStoryToggle = async (req, res, next) => {
+exports.likeStoryToggle = asyncHandler(async (req, res, next) => {
     const user = req.user.id;
     const { storyId } = req.params;
 
@@ -108,34 +96,30 @@ exports.likeStoryToggle = async (req, res, next) => {
         message: 'Please, provide valid storyId.'
     });
 
-    try {
-        const story = await findStoryById(storyId);
-        if (!story) return next({
-            statusCode: STATUS_CODES.NOT_FOUND,
-            message: 'Story not found'
-        });
+    const story = await findStoryById(storyId);
+    if (!story) return next({
+        statusCode: STATUS_CODES.NOT_FOUND,
+        message: 'Story not found'
+    });
 
-        // check if user has already liked
-        if (story.likes.includes(user)) {
-            story.likes.pull(user);
-            await story.save();
-
-            generateResponse(story, 'Story liked removed successfully', res);
-            return;
-        }
-
-        // like the story
-        story.likes.push(user);
+    // check if user has already liked
+    if (story.likes.includes(user)) {
+        story.likes.pull(user);
         await story.save();
 
-        generateResponse(story, 'Story liked successfully', res);
-    } catch (error) {
-        next(error);
+        generateResponse(story, 'Story liked removed successfully', res);
+        return;
     }
-}
+
+    // like the story
+    story.likes.push(user);
+    await story.save();
+
+    generateResponse(story, 'Story liked successfully', res);
+});
 
 // dislike a story
-exports.dislikeStoryToggle = async (req, res, next) => {
+exports.dislikeStoryToggle = asyncHandler(async (req, res, next) => {
     const user = req.user.id;
     const { storyId } = req.params;
 
@@ -144,34 +128,30 @@ exports.dislikeStoryToggle = async (req, res, next) => {
         message: 'Please, provide valid storyId.'
     });
 
-    try {
-        const story = await findStoryById(storyId);
-        if (!story) return next({
-            statusCode: STATUS_CODES.NOT_FOUND,
-            message: 'Story not found'
-        });
+    const story = await findStoryById(storyId);
+    if (!story) return next({
+        statusCode: STATUS_CODES.NOT_FOUND,
+        message: 'Story not found'
+    });
 
-        // check if user has already liked
-        if (story.dislikes.includes(user)) {
-            story.dislikes.pull(user);
-            await story.save();
-
-            generateResponse(story, 'Story disliked removed successfully', res);
-            return;
-        }
-
-        // like the story
-        story.dislikes.push(user);
+    // check if user has already liked
+    if (story.dislikes.includes(user)) {
+        story.dislikes.pull(user);
         await story.save();
 
-        generateResponse(story, 'Story disliked successfully', res);
-    } catch (error) {
-        next(error);
+        generateResponse(story, 'Story disliked removed successfully', res);
+        return;
     }
-}
+
+    // like the story
+    story.dislikes.push(user);
+    await story.save();
+
+    generateResponse(story, 'Story disliked successfully', res);
+});
 
 // add comment to post
-exports.addCommentOnStory = async (req, res, next) => {
+exports.addCommentOnStory = asyncHandler(async (req, res, next) => {
     const body = parseBody(req.body);
 
     // Joi validation
@@ -185,100 +165,86 @@ exports.addCommentOnStory = async (req, res, next) => {
 
     if (req.files?.media?.length > 0) body.media = req.files.media.map(file => file.path);
 
-    try {
-        // if (req?.files?.media?.length > 0) body.media = await s3Uploadv3(req.files?.media);
+    let comment = await createComment(body);
 
-        let comment = await createComment(body);
-
-        // update parent comment
-        if (body.parent) {
-            await updateCommentById(body.parent, { $push: { replies: comment._id } });
-        }
-        await updateStoryById(body.story, { $inc: { commentsCount: 1 } });
-        comment = await getCommentById(comment._id).populate('user', 'firstName lastName profileImage');
-        generateResponse(comment, 'Comment created successfully', res);
-    } catch (error) {
-        next(error);
+    // update parent comment
+    if (body.parent) {
+        await updateCommentById(body.parent, { $push: { replies: comment._id } });
     }
-}
+    await updateStoryById(body.story, { $inc: { commentsCount: 1 } });
+    comment = await getCommentById(comment._id).populate('user', 'firstName lastName profileImage');
+    generateResponse(comment, 'Comment created successfully', res);
+});
 
 // remove comment from post
-exports.removeCommentOnPost = async (req, res, next) => {
+exports.removeCommentOnPost = asyncHandler(async (req, res, next) => {
     const userId = req.user.id;
     const { commentId } = req.params;
 
-    try {
-        // check userId is comment owner
-        const commentObj = await getCommentById(commentId);
-        if (!commentObj) return next({
-            statusCode: STATUS_CODES.NOT_FOUND,
-            message: 'comment not found',
-        });
+    // check userId is comment owner
+    const commentObj = await getCommentById(commentId);
+    if (!commentObj) return next({
+        statusCode: STATUS_CODES.NOT_FOUND,
+        message: 'comment not found',
+    });
 
 
-        if (userId !== commentObj.user.toString()) return next({
-            statusCode: STATUS_CODES.FORBIDDEN,
-            message: "You do not have permission to delete this comment.",
-        });
+    if (userId !== commentObj.user.toString()) return next({
+        statusCode: STATUS_CODES.FORBIDDEN,
+        message: "You do not have permission to delete this comment.",
+    });
 
-        const deletedComment = await removeCommentById(commentId);
-        await updateStoryById(commentObj.story, { $pull: { comments: commentId } });
+    const deletedComment = await removeCommentById(commentId);
+    await updateStoryById(commentObj.story, { $pull: { comments: commentId } });
 
-        generateResponse(deletedComment, 'Comment deleted successfully', res);
-    } catch (error) {
-        next(error);
-    }
-}
+    generateResponse(deletedComment, 'Comment deleted successfully', res);
+});
 
 // get story comments
-exports.getCommentsOfStory = async (req, res, next) => {
+exports.getCommentsOfStory = asyncHandler(async (req, res, next) => {
     const { storyId } = req.params;
 
     const page = parseInt(req.query?.page) || 1;
     const limit = parseInt(req.query?.limit) || 10;
 
-    if (!storyId || !Types.ObjectId.isValid(storyId)) return next({
+    if (!Types.ObjectId.isValid(storyId)) return next({
         statusCode: STATUS_CODES.UNPROCESSABLE_ENTITY,
-        message: 'Please, provide storyId properly.'
+        message: 'invalid storyId.'
     });
 
     const query = { story: new Types.ObjectId(storyId), parent: null };
 
-    try {
-        const commentsData = await getAllComments({
-            query, page, limit,
-            populate: [
-                { path: 'user', select: 'firstName lastName username profileImage' },
-                {
-                    path: 'replies',
-                    populate: [
-                        { path: 'user', select: 'firstName lastName username profileImage' },
-                        {
-                            path: 'replies',
+    const commentsData = await getAllComments({
+        query, page, limit,
+        populate: [
+            { path: 'user', select: 'firstName lastName username profileImage' },
+            {
+                path: 'replies',
+                populate: [
+                    { path: 'user', select: 'firstName lastName username profileImage' },
+                    {
+                        path: 'replies',
 
-                            populate: [
-                                { path: 'user', select: 'firstName lastName username profileImage' },
-                            ]
-                        },
-                    ]
-                },
-            ]
-        });
+                        populate: [
+                            { path: 'user', select: 'firstName lastName username profileImage' },
+                        ]
+                    },
+                ]
+            },
+        ]
+    });
 
-        if (commentsData?.comments?.length === 0) {
-            generateResponse(null, 'No comments found', res);
-            return;
-        }
-
-        commentsData.commentsCount = await countComments({ story: new Types.ObjectId(storyId) });
-        generateResponse(commentsData, 'Comments fetched successfully', res);
-    } catch (error) {
-        next(error);
+    if (commentsData?.comments?.length === 0) {
+        generateResponse(null, 'No comments found', res);
+        return;
     }
-}
+
+    commentsData.commentsCount = await countComments({ story: new Types.ObjectId(storyId) });
+    generateResponse(commentsData, 'Comments fetched successfully', res);
+});
 
 // tag or untag friend in a Story (toggle)
-exports.tagFriendToggle = async (req, res, next) => {
+exports.tagFriendToggle = asyncHandler(async (req, res, next) => {
     const user = req.user.id;
     const { storyId, taggedUserId } = parseBody(req.body);
 
@@ -287,36 +253,32 @@ exports.tagFriendToggle = async (req, res, next) => {
         message: 'Please provide valid storyId and taggedUserId.'
     });
 
-    try {
-        const story = await findStoryById(storyId);
-        if (!story) return next({
-            statusCode: STATUS_CODES.NOT_FOUND,
-            message: 'Story not found'
-        });
+    const story = await findStoryById(storyId);
+    if (!story) return next({
+        statusCode: STATUS_CODES.NOT_FOUND,
+        message: 'Story not found'
+    });
 
-        // check if the current user is the creator of the story
-        if (!story.creator.equals(user)) return next({
-            statusCode: STATUS_CODES.UNAUTHORIZED,
-            message: 'Only the creator of the story can tag or untag friends.'
-        });
+    // check if the current user is the creator of the story
+    if (!story.creator.equals(user)) return next({
+        statusCode: STATUS_CODES.UNAUTHORIZED,
+        message: 'Only the creator of the story can tag or untag friends.'
+    });
 
-        // check if the user is already tagged or not
-        if (story.tag.includes(taggedUserId)) {
-            story.tag.pull(taggedUserId);
-            await story.save();
-            generateResponse(story, 'User tag removed successfully from the story', res);
-            return;
-        }
-
-        // tag the user in the story
-        story.tag.push(taggedUserId);
+    // check if the user is already tagged or not
+    if (story.tag.includes(taggedUserId)) {
+        story.tag.pull(taggedUserId);
         await story.save();
-
-        generateResponse(story, 'User tagged Successfully for the story', res);
-    } catch (error) {
-        next(error);
+        generateResponse(story, 'User tag removed successfully from the story', res);
+        return;
     }
-}
+
+    // tag the user in the story
+    story.tag.push(taggedUserId);
+    await story.save();
+
+    generateResponse(story, 'User tagged Successfully for the story', res);
+});
 
 // toggle isHidden field of a story
 exports.toggleStoryVisibility = asyncHandler(async (req, res, next) => {

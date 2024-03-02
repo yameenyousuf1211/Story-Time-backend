d98741e3a9a0673bbbaa6a24bd2e0e708e19046d
Shahzad <shazee257@gmail.com>
2024-03-02 07:59:53 +0500

    refactor dbConfig and add colors package

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+const colors = require('colors');
 
-module.exports = () => {
-    mongoose.set('strictQuery', true);
-    mongoose.connect(process.env.MONGODB_URL)
-        .then(() => console.log('Connected to DB -> ', process.env.MONGODB_URL.split('/')[3]))
-        .catch((error) => console.log("db error: ", error));
+module.exports = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URL);
+        console.log(`MongoDB Connected -> : ${conn.connection.name}`.magenta.bold);
+    } catch (error) {
+        console.error(`Error: ${error.message}`.red.bold);
+        process.exit(1);
+    }
 }

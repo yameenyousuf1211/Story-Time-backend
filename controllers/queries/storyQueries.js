ec5c1e0a4a35d58db37de211c2d50ffdc90c13be
Usman Nazir <usmannazir428@gmail.com>
2024-03-13 03:17:38 +0500

    Feed Query Updated

--- controllers/queries/storyQueries.js
+++ controllers/queries/storyQueries.js
@@ -6,39 +6,31 @@ exports.getStoriesQuery = (user) => {
         {
             $lookup: {
                 from: 'followings',
-                let: { user: new Types.ObjectId(user), contributorIds: '$contributors' },
+                let: { user: new Types.ObjectId(user), contributorIds: '$contributors', sharedBy: '$sharedBy' },
                 pipeline: [
                     {
                         $match: {
                             $expr: {
-                                $and: [
-                                    { $eq: ['$user', '$$user'] },
-                                    { $in: ['$following', '$$contributorIds'] },
-                                ],
-                            },
+                                $or: [
+                                    {
+                                        $and: [
+                                            { $eq: ['$user', '$$user'] },
+                                            { $in: ['$following', '$$contributorIds'] },
+                                        ]
+                                    },
+                                    {
+                                        $and: [
+                                            { $eq: ['$user', '$$user'] },
+                                            { $eq: ['$following', '$$sharedBy'] },
+                                        ]
+                                    }
+                                ]
+                            }
+
                         },
                     },
                 ],
                 as: 'followings',
-            },
-        },
-        {
-            $lookup: {
-                from: 'followings',
-                let: { user: new Types.ObjectId(user), sharedBy: '$sharedBy' },
-                pipeline: [
-                    {
-                        $match: {
-                            $expr: {
-                                $and: [
-                                    { $eq: ['$user', '$$user'] },
-                                    { $eq: ['$following', '$$sharedBy'] },
-                                ],
-                            },
-                        },
-                    },
-                ],
-                as: 'sharedByFollowings',
             },
         },
         {
@@ -48,26 +40,25 @@ exports.getStoriesQuery = (user) => {
                 likedByMe: { $in: [new Types.ObjectId(user), "$likes"] },
                 dislikesByMe: { $in: [new Types.ObjectId(user), "$dislikes"] },
                 isFollowing: { $cond: [{ $gt: [{ $size: '$followings' }, 0] }, true, false] },
-                isSharedByFollowing: { $cond: [{ $gt: [{ $size: '$sharedByFollowings' }, 0] }, true, false] },
-                isTagged: { $in: [new Types.ObjectId(user), "$tag"] },
             }
         },
         {
             $match: {
                 $or: [
-                    { isFollowing: true },
-                    { isTagged: true },
-                    { isSharedByFollowing: true },
-                    { contributors: new Types.ObjectId(user) }
+                    { isFollowing: true }, // if user is following the contributor or the sharedBy
+                    { sharedBy: new Types.ObjectId(user) },
+                    { contributors: new Types.ObjectId(user) },
+                    { tag: new Types.ObjectId(user) },
                 ],
             },
         },
         { $lookup: { from: "users", localField: "creator", foreignField: "_id", as: "creator" } }, { $unwind: "$creator" },
         { $lookup: { from: "categories", localField: "subCategory", foreignField: "_id", as: "subCategory" } }, { $unwind: "$subCategory" },
         { $sort: { createdAt: -1 } },    // latest first
-        { $project: { followings: 0, sharedByFollowings: 0 } }
+        { $project: { followings: 0 } }
     ]
 }
+
 // get user's stories
 exports.getUserStoriesQuery = (user, type, isHidden) => {
     const pipeline = [

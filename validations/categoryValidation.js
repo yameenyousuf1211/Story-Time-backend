69bd34114b1475d79dbdf6c4753f77c82e5c12a1
Muhammad Usman Nazir <usmannazir428@gmail.com>
2024-04-21 02:14:00 +0500

    fixed create category API

--- validations/categoryValidation.js
+++ validations/categoryValidation.js
@@ -3,9 +3,7 @@ const Joi = require('joi');
 exports.createCategoryValidation = Joi.object({
   name: Joi.string().required(),
   parent: Joi.string(), // Assuming parent can be the ID of another category
-  hexCode: Joi.string().allow(null, '').regex(/^#[0-9a-fA-F]{6}$/).messages({
-    'string.pattern.base': 'hexCode is not valid.',
-  }).optional(),
+  hexCode: Joi.string().regex(/^#[0-9a-fA-F]{6}$/).required(),
 });
 
 exports.updateCategoryValidation = Joi.object({

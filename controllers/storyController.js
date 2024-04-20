37d2c2fb27b238ab6759ff01da05af7e3c751a17
Muhammad Usman Nazir <usmannazir428@gmail.com>
2024-04-21 02:10:24 +0500

    Only the contributor of the story can change the visibility

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -297,7 +297,7 @@ exports.toggleStoryVisibility = asyncHandler(async (req, res, next) => {
     });
 
     // check if the current user is the contributor of the story
-    if (!story.contributors.some(contributor => contributor.equals(user))) return next({
+    if (story.contributors.find(contributor => contributor.toString() != user)) return next({ // array.find returns the element, not the index
         statusCode: STATUS_CODES.UNAUTHORIZED,
         message: 'Only the contributor of the story can change the visibility.'
     });

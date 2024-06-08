26f97faa23df95b141f0b3c1eb919ba67cd3576c
Muhammad Usman Nazir <usmannazir428@gmail.com>
2024-06-09 03:37:56 +0500

    refactor: Update category validation to make hexCode optional

--- validations/categoryValidation.js
+++ validations/categoryValidation.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 exports.createCategoryValidation = Joi.object({
   name: Joi.string().required(),
   parent: Joi.string(), // Assuming parent can be the ID of another category
-  hexCode: Joi.string().regex(/^#[0-9a-fA-F]{6}$/).required(),
+  hexCode: Joi.string().regex(/^#[0-9a-fA-F]{6}$/)
 });
 
 exports.updateCategoryValidation = Joi.object({

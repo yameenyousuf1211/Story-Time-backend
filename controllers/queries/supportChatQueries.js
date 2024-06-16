a9117830827ad77c35ed67293f723c7f73dc4f8a
Shahzad <shazee257@gmail.com>
2024-06-16 13:33:33 +0500

    sendMessage, getChatList, & lookUp User method implemented

--- controllers/queries/supportChatQueries.js
+++ controllers/queries/supportChatQueries.js
@@ -1,37 +1,52 @@
-const { getMongoId } = require("../../utils");
+const { getMongoId, lookupUser } = require("../../utils");
 
-exports.getChatsQuery = async (user, search = "") => {
+exports.getChatsQuery = async (userId, search = "") => {
     return [
-        { $match: { $or: [{ receiver: getMongoId(user) }, { sender: getMongoId(user) }] } },
+        { $match: { $or: [{ receiver: getMongoId(userId) }, { sender: getMongoId(userId) }] } },
         {
             $group: {
                 _id: "$chat",
                 unreadMessages: {
                     $sum: {
-                        $cond: [{ $and: [{ $eq: ["$receiver", getMongoId(user)] }, { $eq: ["$isRead", false] }] }, 1, 0]
+                        $cond: [{ $and: [{ $eq: ["$receiver", getMongoId(userId)] }, { $eq: ["$isRead", false] }] }, 1, 0]
                     }
                 }
             },
         },
         // look up chat
-        { $lookup: { from: "supportchat", localField: "_id", foreignField: "_id", as: "chat" } }, { $unwind: "$chat" },
+        { $lookup: { from: "supportchats", localField: "_id", foreignField: "_id", as: "chat" } }, { $unwind: "$chat" },
 
-        // lookup users under chat
-        {
-            $lookup: {
-                from: "users",
-                localField: "chat.users",
-                foreignField: "_id",
-                as: "users",
-            },
-        },
+        ...lookupUser("chat.users.0", "user1"),
+        ...lookupUser("chat.users.1", "user2"),
 
-        // match users using $regex search
+        // Match users using $regex search only if the user is not the current user
         {
             $match: {
                 $or: [
-                    { "users.firstName": { $regex: search, $options: "i" } },
-                    { "users.lastName": { $regex: search, $options: "i" } },
+                    {
+                        $and: [
+                            { "chat.users.0": { $ne: getMongoId(userId) } },
+                            { "user1.firstName": { $regex: search, $options: "i" } },
+                        ]
+                    },
+                    {
+                        $and: [
+                            { "chat.users.0": { $ne: getMongoId(userId) } },
+                            { "user1.lastName": { $regex: search, $options: "i" } },
+                        ]
+                    },
+                    {
+                        $and: [
+                            { "chat.users.1": { $ne: getMongoId(userId) } },
+                            { "user2.firstName": { $regex: search, $options: "i" } },
+                        ]
+                    },
+                    {
+                        $and: [
+                            { "chat.users.1": { $ne: getMongoId(userId) } },
+                            { "user2.lastName": { $regex: search, $options: "i" } },
+                        ]
+                    },
                 ],
             },
         },

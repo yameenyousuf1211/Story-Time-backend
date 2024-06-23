f75994ab60f7af91e3f4d2a98b06df093744d9ce
Muhammad Usman Nazir <usmannazir428@gmail.com>
2024-06-24 02:26:42 +0500

    updated middleware to verify resetToken for admin when resetting the password

--- api/authApi.js
+++ api/authApi.js
@@ -29,7 +29,6 @@ class AuthAPI {
         router.put('/reset-password', authMiddleware(Object.values(ROLES)), resetPassword);
         router.put('/refresh-token', getRefreshToken);
         router.put('/forget-password', sendResetLink);
-        router.put('/verify-reset-token', verifyResetToken);
 
     }
 

51715f3e0102343fd546463c1f6b64f5c9d11234
Shahzad <shazee257@gmail.com>
2024-06-26 12:27:14 +0500

    firebase push notifications implemented & test API for push notification created

--- utils/index.js
+++ utils/index.js
@@ -3,23 +3,10 @@ const fs = require('fs');
 const { sign } = require('jsonwebtoken');
 const { default: mongoose } = require('mongoose');
 const firebase = require("firebase-admin");
+const serviceAccount = require("../firebase.json");
 
-let firebaseApp;
-const serviceAccount = {
-    "type": process.env.FIREBASE_TYPE,
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "client_id": process.env.FIREBASE_CLIENT_ID,
-    "auth_uri": process.env.FIREBASE_AUTH_URI,
-    "token_uri": process.env.FIREBASE_TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN
-}
-
-if (process.env.FIREBASE_PRIVATE_KEY) firebaseApp = firebase.initializeApp({ credential: firebase.credential.cert(serviceAccount) });
+console.log('serviceAccount', serviceAccount);
+const firebaseApp = firebase.initializeApp({ credential: firebase.credential.cert(serviceAccount) });
 
 // Response generation utility
 exports.generateResponse = (data, message, res, code = 200) => {

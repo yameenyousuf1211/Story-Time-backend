83642228dfc80a8f2ae47a2b0fa6ef8c75f1a734
Muhammad Usman Nazir <usmannazir428@gmail.com>
2024-08-08 00:50:43 +0500

    Refactor error handling middleware

--- middlewares/errorHandling.js
+++ middlewares/errorHandling.js
@@ -1,17 +1,3 @@
-const { Schema, model } = require("mongoose");
-
-// error log schema
-const logSchema = new Schema({
-    timestamp: { type: Date, default: Date.now },
-    body: { type: String },
-    statusCode: { type: Number },
-    endPoint: { type: String },
-    message: String,
-    stack: String,
-});
-
-const LogModel = model('Log', logSchema);
-
 class ErrorHandling {
     static notFound(req, res, next) {
         const error = new Error(`Not Found - ${req.originalUrl}`);
@@ -22,21 +8,6 @@ class ErrorHandling {
     static errorHandler(err, req, res, next) {
         const statusCode = err?.statusCode ? err?.statusCode : 500;
         const error = new Error(err?.message.replace(/\"/g, '') || 'Internal Server Error');
-
-        const log = new LogModel({
-            body: JSON.stringify(req.body),
-            statusCode,
-            endPoint: req?.originalUrl,
-            message: error?.message,
-            stack: error?.stack,
-        });
-
-        // save the log document
-        try {
-            log.save();
-        } catch (error) {
-            console.log("Error saving log: ", error);
-        }
 
         return res.status(statusCode).json({
             message: error?.message,

3e10aaf295aea0d78ac8a719dc0514e7e694bd82
yameen <46494345+yameenyousuf1211@users.noreply.github.com>
2024-08-16 10:56:24 -0700

    add localhost to cors

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(cookieSession({
 }));
 
 app.use(cors({
-    origin: ["https://admin.storytime.social"], credentials: true
+    origin: ["https://admin.storytime.social","http://localhost:3000"], credentials: true
 }));
 
 app.get('/', (req, res) => res.json({ message: `Welcome to the ${process.env.APP_NAME} Project` }));
